d069fb67ea794c7cd2d9423b7bca7facd12722ec
Miguel <Miguel@DESKTOP-G473HQ9>
2022-09-07 10:48:08 +0200

    Version 1.0.0

---
+++ miguelRamirez_proyectoGit/src/miguelRamirez_proyectoGit/main_git.java
@@ -0,0 +1,9 @@
+package miguelRamirez_proyectoGit;
+
+public class main_git {
+	// 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
+	public static void main(String[] args) {
+		System.out.println("Hola mundo.");
+	}
+
+}
